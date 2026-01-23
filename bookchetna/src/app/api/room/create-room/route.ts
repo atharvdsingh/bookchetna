13965539468182cd90ca89c9a1cbb63099249891
atharvdsingh <dsingh9450492600@gmail.com>
2026-01-23 12:13:51 +0530

    feature:server routes for room

--- bookchetna/src/app/api/room/create-room/route.ts
+++ bookchetna/src/app/api/room/create-room/route.ts
@@ -1,20 +1,30 @@
 import { AppError } from "@/util/AppError";
 import { handleApiError } from "@/util/HandleError";
-import type { NextRequest } from "next/server";
+import { prisma } from "@/util/Prisma";
+import { NextResponse, type NextRequest } from "next/server";
 
 
 export default async function POST(req:NextRequest){
     try {
-
         const body= await req.json()
         if(!body.roomName ){
             throw new AppError("room name is missing",401)
         }
-        
-        
+        const respons=await prisma.room.create({
+            data:{
+                roomName:body.roomName,
+                discription:body.discription!
+            }
+        })
+        if(!respons){
+            throw new AppError(
+    "something went wront",500)
+        }
+        return NextResponse.json({
+            message:"room created successfully",success:true
+        },{status:200})        
     } catch (error) {
         handleApiError(error)
         
     }
-
 }
---
+++ bookchetna/src/app/api/room/join-room/route.ts
@@ -0,0 +1,38 @@
+import { AppError } from "@/util/AppError";
+import { handleApiError } from "@/util/HandleError";
+import { prisma } from "@/util/Prisma";
+import { getServerSession } from "next-auth";
+import { NextResponse, type NextRequest } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+export default async function  POST(req:NextRequest){
+    try {
+        const body= await req.json()
+        const session=await getServerSession(authOptions)
+        if(!session?.user.id){
+            throw new AppError("Envalied session",400)
+        }
+        if(!body.roomId){
+            throw new AppError("room id is missing",402)
+        }
+        const respons=await prisma.roomMembership.create({
+            data:{
+                memberId:session.user.id,
+                roomId:body.roomId
+            }
+        })
+        if(!respons){
+            throw new AppError("something went wrong",500)
+        }
+        return NextResponse.json({
+            message:"joined room",success:true
+        },{
+            status:200
+        })
+        
+    } catch (error) {
+        handleApiError(error)
+        
+    }
+
+}
